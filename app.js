d38f89e0f9e3495d9d12ffa39e7a9f0ad808db14
Mark Libario <mkelvin.libario@gmail.com>
2018-10-26 21:16:09 -0400

    setting up dice variable
    use Math.random()

--- app.js
+++ app.js
@@ -6,8 +6,10 @@ GAME RULES:
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 */
-let scores, roundScore, activePlayer;
+let scores, roundScore, activePlayer, dice;
 
 scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
